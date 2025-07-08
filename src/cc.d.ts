9a68fe3e5d78473e8d581486115f203878ce3d7f
lsc0103 <lsc0103@outlook.com>
2025-07-08 09:30:48 +0800

    feat: make entry script a Cocos component

---
+++ src/cc.d.ts
@@ -0,0 +1,4 @@
+declare module 'cc' {
+  export class Component {}
+  export const _decorator: any;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
+import { _decorator, Component } from 'cc';
 import { GameManager } from './core/GameManager';
 import { UIManager } from './core/UIManager';
 import { Levels } from './levels/LevelData';
 
-const ui = new UIManager();
-const game = GameManager.instance;
+const { ccclass } = _decorator;
 
-ui.showMainMenu();
+/**
+ * Entry component for the demo project.
+ *
+ * Attach this script to a node in your scene to trigger the basic
+ * bootstrap logic when the scene loads.
+ */
+@ccclass('Index')
+export class Index extends Component {
+  start() {
+    const ui = new UIManager();
+    const game = GameManager.instance;
 
-game.startLevel(Levels[0].id, Levels[0].cats);
-ui.showLevelUI(game.catsLeft, game.currentLevel);
+    ui.showMainMenu();
+
+    game.startLevel(Levels[0].id, Levels[0].cats);
+    ui.showLevelUI(game.catsLeft, game.currentLevel);
+  }
+}
+
+export default null;
